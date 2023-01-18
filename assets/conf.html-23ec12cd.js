8600b2506499fbda57de95e357b525124a3623a8
bestruirui <1004279909@qq.com>
2023-01-18 21:30:28 +0800

    2023-01-18-21:30

---
+++ assets/conf.html-23ec12cd.js
@@ -0,0 +1,39 @@
+import{ab as s,C as n,D as a,ad as e}from"./framework-e618a78f.js";const i={},t=e(`<h2 id="反向代理某个端口" tabindex="-1"><a class="header-anchor" href="#反向代理某个端口" aria-hidden="true">#</a> 反向代理某个端口</h2><h4 id="https" tabindex="-1"><a class="header-anchor" href="#https" aria-hidden="true">#</a> HTTPS</h4><div class="language-nginx line-numbers-mode" data-ext="nginx"><pre class="language-nginx"><code><span class="token directive"><span class="token keyword">server</span></span> <span class="token punctuation">{</span>
+    <span class="token directive"><span class="token keyword">listen</span>                  <span class="token number">443</span> ssl http2</span><span class="token punctuation">;</span>
+    <span class="token directive"><span class="token keyword">listen</span>                  [::]:443 ssl http2</span><span class="token punctuation">;</span>
+    <span class="token directive"><span class="token keyword">server_name</span>             bestrui.top</span><span class="token punctuation">;</span>
+
+    <span class="token comment"># SSL</span>
+    <span class="token directive"><span class="token keyword">ssl_certificate</span>         /nginx/ssl/jd.bestrui.top_nginx/jd.bestrui.top_bundle.crt</span><span class="token punctuation">;</span>
+    <span class="token directive"><span class="token keyword">ssl_certificate_key</span>     /nginx/ssl/jd.bestrui.top_nginx/jd.bestrui.top.key</span><span class="token punctuation">;</span>
+    <span class="token directive"><span class="token keyword">ssl_session_timeout</span> <span class="token number">5m</span></span><span class="token punctuation">;</span>
+    <span class="token directive"><span class="token keyword">ssl_protocols</span> TLSv1.2 TLSv1.3</span><span class="token punctuation">;</span>
+    <span class="token directive"><span class="token keyword">ssl_ciphers</span> ECDHE-RSA-AES128-GCM-SHA256:HIGH:!aNULL:!MD5:!RC4:!DHE</span><span class="token punctuation">;</span> 
+    <span class="token directive"><span class="token keyword">ssl_prefer_server_ciphers</span> <span class="token boolean">on</span></span><span class="token punctuation">;</span>
+
+    <span class="token comment"># reverse proxy</span>
+    <span class="token directive"><span class="token keyword">location</span> /</span> <span class="token punctuation">{</span>
+        <span class="token directive"><span class="token keyword">proxy_pass</span>            http://192.168.31.20:15000</span><span class="token punctuation">;</span>
+        <span class="token directive"><span class="token keyword">proxy_set_header</span> Host <span class="token variable">$host</span></span><span class="token punctuation">;</span>
+    <span class="token punctuation">}</span>
+<span class="token punctuation">}</span>
+
+<span class="token comment"># HTTP redirect</span>
+<span class="token directive"><span class="token keyword">server</span></span> <span class="token punctuation">{</span>
+    <span class="token directive"><span class="token keyword">listen</span>      <span class="token number">80</span></span><span class="token punctuation">;</span>
+    <span class="token directive"><span class="token keyword">listen</span>      [::]:80</span><span class="token punctuation">;</span>
+    <span class="token directive"><span class="token keyword">server_name</span> bestrui.top</span><span class="token punctuation">;</span>
+    <span class="token directive"><span class="token keyword">location</span> /</span> <span class="token punctuation">{</span>
+        <span class="token directive"><span class="token keyword">return</span> <span class="token number">301</span> https://bestrui.top<span class="token variable">$request_uri</span></span><span class="token punctuation">;</span>
+    <span class="token punctuation">}</span>
+<span class="token punctuation">}</span>
+
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h4 id="http" tabindex="-1"><a class="header-anchor" href="#http" aria-hidden="true">#</a> HTTP</h4><div class="language-nginx line-numbers-mode" data-ext="nginx"><pre class="language-nginx"><code><span class="token directive"><span class="token keyword">server</span></span><span class="token punctuation">{</span>
+        <span class="token directive"><span class="token keyword">listen</span> [::]:80</span><span class="token punctuation">;</span>
+        <span class="token directive"><span class="token keyword">listen</span> 0.0.0.0:80</span><span class="token punctuation">;</span>
+        <span class="token directive"><span class="token keyword">server_name</span> bestrui.top</span><span class="token punctuation">;</span>
+        <span class="token directive"><span class="token keyword">location</span> = /</span> <span class="token punctuation">{</span>
+          <span class="token directive"><span class="token keyword">proxy_pass</span>         http://192.168.31.3:3001</span><span class="token punctuation">;</span>
+        <span class="token punctuation">}</span>
+<span class="token punctuation">}</span>
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,5),p=[t];function c(l,o){return n(),a("div",null,p)}const r=s(i,[["render",c],["__file","conf.html.vue"]]);export{r as default};
---
+++ assets/index.html-f6799b97.js
@@ -0,0 +1,1 @@
+import{ab as t,C as a,D as c,A as e,O as n}from"./framework-e618a78f.js";const r={},s=e("h2",{id:"常用软件",tabindex:"-1"},[e("a",{class:"header-anchor",href:"#常用软件","aria-hidden":"true"},"#"),n(" 常用软件")],-1),o=[s];function d(_,i){return a(),c("div",null,o)}const l=t(r,[["render",d],["__file","index.html.vue"]]);export{l as default};
