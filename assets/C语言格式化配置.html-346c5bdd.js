ffcdb9aab534ac65ba527c3b39463840c692006e
bestruirui <1004279909@qq.com>
2024-01-05 20:36:52 +0800

    2024-01-05-20:36

--- assets/C语言格式化配置.html-346c5bdd.js
+++ assets/C语言格式化配置.html-346c5bdd.js
@@ -1,36 +1,20 @@
-import{ab as n,C as s,D as a,ad as e}from"./framework-e618a78f.js";const t={},o=e(`<div class="language-json line-numbers-mode" data-ext="json"><pre class="language-json"><code><span class="token punctuation">{</span>
+import{ab as n,C as s,D as a,ad as e}from"./framework-e618a78f.js";const t={},o=e(`<p>在项目根目录下新建<code>.vscode/settings.json</code> 将以下内容复制到新建的文件中</p><div class="language-json line-numbers-mode" data-ext="json"><pre class="language-json"><code><span class="token punctuation">{</span>
     <span class="token property">&quot;files.autoSave&quot;</span><span class="token operator">:</span> <span class="token string">&quot;onFocusChange&quot;</span><span class="token punctuation">,</span>
-
     <span class="token property">&quot;editor.formatOnSave&quot;</span><span class="token operator">:</span> <span class="token boolean">true</span><span class="token punctuation">,</span>
-
     <span class="token property">&quot;C_Cpp.formatting&quot;</span><span class="token operator">:</span> <span class="token string">&quot;vcFormat&quot;</span><span class="token punctuation">,</span>
-
     <span class="token property">&quot;C_Cpp.vcFormat.newLine.beforeOpenBrace.block&quot;</span><span class="token operator">:</span> <span class="token string">&quot;newLine&quot;</span><span class="token punctuation">,</span>
-
     <span class="token property">&quot;C_Cpp.vcFormat.newLine.beforeOpenBrace.lambda&quot;</span><span class="token operator">:</span> <span class="token string">&quot;newLine&quot;</span><span class="token punctuation">,</span>
-
     <span class="token property">&quot;C_Cpp.vcFormat.newLine.beforeOpenBrace.namespace&quot;</span><span class="token operator">:</span> <span class="token string">&quot;newLine&quot;</span><span class="token punctuation">,</span>
-
     <span class="token property">&quot;C_Cpp.vcFormat.newLine.beforeOpenBrace.type&quot;</span><span class="token operator">:</span> <span class="token string">&quot;newLine&quot;</span><span class="token punctuation">,</span>
-
     <span class="token property">&quot;C_Cpp.vcFormat.indent.preserveWithinParentheses&quot;</span><span class="token operator">:</span> <span class="token boolean">true</span><span class="token punctuation">,</span>
-
     <span class="token property">&quot;C_Cpp.vcFormat.newLine.scopeBracesOnSeparateLines&quot;</span><span class="token operator">:</span> <span class="token boolean">true</span><span class="token punctuation">,</span>
-
     <span class="token property">&quot;C_Cpp.vcFormat.space.afterCastCloseParenthesis&quot;</span><span class="token operator">:</span> <span class="token boolean">true</span><span class="token punctuation">,</span>
-
     <span class="token property">&quot;C_Cpp.vcFormat.space.aroundAssignmentOperator&quot;</span><span class="token operator">:</span> <span class="token string">&quot;ignore&quot;</span><span class="token punctuation">,</span>
-
     <span class="token property">&quot;C_Cpp.vcFormat.space.aroundBinaryOperator&quot;</span><span class="token operator">:</span> <span class="token string">&quot;ignore&quot;</span><span class="token punctuation">,</span>
-
     <span class="token property">&quot;C_Cpp.vcFormat.newLine.beforeOpenBrace.function&quot;</span><span class="token operator">:</span> <span class="token string">&quot;newLine&quot;</span><span class="token punctuation">,</span>
-
     <span class="token property">&quot;C_Cpp.vcFormat.newLine.beforeElse&quot;</span><span class="token operator">:</span> <span class="token boolean">true</span><span class="token punctuation">,</span>
-
     <span class="token property">&quot;C_Cpp.vcFormat.indent.withinParentheses&quot;</span><span class="token operator">:</span> <span class="token string">&quot;alignToParenthesis&quot;</span><span class="token punctuation">,</span>
-
     <span class="token property">&quot;C_Cpp.vcFormat.indent.preserveComments&quot;</span><span class="token operator">:</span> <span class="token boolean">true</span><span class="token punctuation">,</span>
-
     <span class="token property">&quot;C_Cpp.vcFormat.space.beforeComma&quot;</span><span class="token operator">:</span> <span class="token boolean">true</span><span class="token punctuation">,</span>
 <span class="token punctuation">}</span>
-</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,1),p=[o];function r(i,c){return s(),a("div",null,p)}const u=n(t,[["render",r],["__file","C语言格式化配置.html.vue"]]);export{u as default};
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,2),p=[o];function r(c,l){return s(),a("div",null,p)}const u=n(t,[["render",r],["__file","C语言格式化配置.html.vue"]]);export{u as default};
